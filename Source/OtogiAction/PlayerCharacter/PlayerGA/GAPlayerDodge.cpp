cc03e5acb7493362c119d3b74f007cc6a7d1a583
sinyuto12345-prog <sinyuto12345@gmail.com>
2026-07-31 11:40:58 +0900

    つらい抜けます

--- Source/OtogiAction/PlayerCharacter/PlayerGA/GAPlayerDodge.cpp
+++ Source/OtogiAction/PlayerCharacter/PlayerGA/GAPlayerDodge.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GAPlayerDodge.h"
 #include "EnhancedInputSubsystems.h"
 #include "GameFramework/PlayerController.h"
